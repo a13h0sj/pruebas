ddaa4440e596e556273691e021a8a702cbe23d51
a13h0sj <69475332+a13h0sj@users.noreply.github.com>
2020-08-10 19:30:12 -0500

    ASD
    ASD

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express= require('express');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
 const app = express();
 app.get('*',(req,res)=>{
     console.log('asdadqwe');
